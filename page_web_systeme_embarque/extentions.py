c9bce8d086dbfa6c7216e75d50ca481fe9ac948f
SteveElouga <nyobeelouga5@gmail.com>
2024-10-04 22:54:34 +0100

    Mise a jour de l'application

--- page_web_systeme_embarque/extentions.py
+++ page_web_systeme_embarque/extentions.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -8,3 +9,4 @@ app.config.from_object("config")
 
 db = SQLAlchemy()
 migrate = Migrate()
+CORS(app)
